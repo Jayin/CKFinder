35a0c1ecb226ea5a3a778fda6cc7ac6c9f5817d1
Jayin Ton <tonjayin@gmail.com>
2016-04-06 16:49:57 +0800

    create ztb

---
+++ skins/ztb/skin.js
@@ -0,0 +1,18 @@
+CKFinder.define( {
+    config: function( config ) {
+        // Use sample custom theme.
+        config.themeCSS = 'skins/ztb/ckfinder.css';
+
+        // Use Moono skin icons.
+        config.iconsCSS = 'skins/ztb/icons.css';
+
+        return config;
+    },
+
+    init: function( finder ) {
+        CKFinder.require( [ 'jquery' ], function( jQuery ) {
+            // Enforce black iconset.
+            jQuery( 'body' ).addClass( 'ui-alt-icon' );
+        } );
+    }
+} );
