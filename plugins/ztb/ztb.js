35eb742668cbd5e74078f6b1a5aaed96004b4dd7
Jayin Ton <tonjayin@gmail.com>
2016-04-13 21:50:51 +0800

    format

--- plugins/ztb/ztb.js
+++ plugins/ztb/ztb.js
@@ -1,27 +1,24 @@
-CKFinder.define( function () {
+CKFinder.define(function() {
 
-     var MyPlugin = {
-         init: function ( finder ) {
-             //点击文件夹就展开             
-             finder.on( 'folder:selected', function ( evt ) {
-               $('.ui-btn-active').next().click()
-             } );
+  var MyPlugin = {
+    init: function(finder) {
+      //点击文件夹就展开             
+      finder.on('folder:selected', function(evt) {
+        $('.ui-btn-active').next().click();
+      });
 
-             finder.on( 'folder:getFiles:after', function(evt){
-                console.log('folder:getFiles:after======!!!')
-                console.log(evt)
-                var $descs = $('.ckf-file-desc p');
-                console.log($descs.length)
-                for(var i = 0; i<$descs.length;i++){
-                  var e = $descs[i];
-                  var outerHTML = e.outerHTML;
-                  var innerHTML = e.innerHTML;
-                  var sp = innerHTML.split('<br>');
-                  e.innerHTML = '<span class="jy-date">' + sp[0] + '</span><span class="jy-size" style="float: right;">' + sp[1] + '</span>';
-                }
-             });
-         }
-     };
+      finder.on('folder:getFiles:after', function(evt) {
+        var $descs = $('.ckf-file-desc p');
+        for (var i = 0; i < $descs.length; i++) {
+          var e = $descs[i];
+          var outerHTML = e.outerHTML;
+          var innerHTML = e.innerHTML;
+          var sp = innerHTML.split('<br>');
+          e.innerHTML = '<span class="jy-date">' + sp[0] + '</span><span class="jy-size" style="float: right;">' + sp[1] + '</span>';
+        }
+      });
+    }
+  };
 
-     return MyPlugin;
- } );
+  return MyPlugin;
+});
