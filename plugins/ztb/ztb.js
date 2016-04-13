cf85bbbd69aca7b5c62c2cbf6cb6c390ecc2cd12
Jayin Ton <tonjayin@gmail.com>
2016-04-13 20:56:32 +0800

    update 文件描述：容量在右侧 rel #5

--- plugins/ztb/ztb.js
+++ plugins/ztb/ztb.js
@@ -3,10 +3,23 @@ CKFinder.define( function () {
      var MyPlugin = {
          init: function ( finder ) {
              //点击文件夹就展开             
-             finder.on( 'folder:selected', function ( evt ,e) {
+             finder.on( 'folder:selected', function ( evt ) {
                $('.ui-btn-active').next().click()
              } );
-             
+
+             finder.on( 'folder:getFiles:after', function(evt){
+                console.log('folder:getFiles:after======!!!')
+                console.log(evt)
+                var $descs = $('.ckf-file-desc p');
+                console.log($descs.length)
+                for(var i = 0; i<$descs.length;i++){
+                  var e = $descs[i];
+                  var outerHTML = e.outerHTML;
+                  var innerHTML = e.innerHTML;
+                  var sp = innerHTML.split('<br>');
+                  e.innerHTML = '<span class="jy-date">' + sp[0] + '</span><span class="jy-size" style="float: right;">' + sp[1] + '</span>';
+                }
+             });
          }
      };
 
