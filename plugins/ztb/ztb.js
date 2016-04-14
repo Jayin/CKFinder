cb4722574403851c0332a069a61e8696f4b3a966
Jayin Ton <tonjayin@gmail.com>
2016-04-14 11:26:34 +0800

    文件上传状态改变: 成功：绿  失败：红 一般：黑 close #5

--- plugins/ztb/ztb.js
+++ plugins/ztb/ztb.js
@@ -17,6 +17,30 @@ CKFinder.define(function() {
           e.innerHTML = '<span class="jy-date">' + sp[0] + '</span><span class="jy-size" style="float: right;">' + sp[1] + '</span>';
         }
       });
+
+      //文件上传状态改变: 成功：绿  失败：红 一般：黑
+      finder.on('app:ready', function(evt) {
+        var $upload_container = $('#ckf-98');
+        $('#ckf-98').bind('DOMSubtreeModified', function() {
+          var $update_status = $('.ckf-upload-status');
+          if ($update_status.innerHTML === '') {
+            // console.log('remove...')
+          } else {
+            if ($update_status.text().indexOf('错误') !== -1) {
+              $update_status.css('color', '#fa425d'); //red
+              // console.log('red..')
+              return;
+            }
+            if ($update_status.text().indexOf('完成') !== -1) {
+              $update_status.css('color', 'green');
+              // console.log('green..')
+              return;
+            }
+            // console.log('black..')
+            $update_status.css('color', 'black');
+          }
+        });
+      });
     }
   };
 
